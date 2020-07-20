867bf42f2e1288579cf1b584c5b6d22f8f5648f2
Amalia Hay <amaliawhay@gmail.com>
2020-07-19 22:02:06 -0700

    Restructured objects and functions again. file successfully created

--- Develop/app.js
+++ Develop/app.js
@@ -8,243 +8,322 @@ const util = require("util");
 
 const asyncWriteFile = util.promisify(fs.writeFile);
 
-const employees = [];
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 //Questions to be asked of all employee types
-function runTeamBuilder() {
-  console.log(
-    "Build profiles for your team members! Answer the following questions about each of your team members"
-  );
-  function roleQ() {
-    inquirer
-      .prompt([
-        {
-          type: "checkbox",
-          name: "role",
-          message:
-            "Which of the following roles does the employee have?",
-          choices: ["Engineer", "Manager", "Intern"],
-        },
-      ])
-      .then((ans) => {
-        if (ans.role[0] === "Engineer") {
-          engineerQ();
-        } else if (ans.role[0] === "Manager") {
-          managerQ();
-        } else {
-          internQ();
-        }
-      });
-  }
-  function engineerQ() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the full name of this engineer?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the engineer's ID number?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the engineer's email address?",
-      },
-      {
-        type: "input",
-        name: "github",
-        message:
-          "Please enter the engineer's GitHub profile URL",
-      },
-    ]);
-  }
-  function managerQ() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the full name of this manager?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the manager's ID number?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the manager's email address?",
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the manager's office number?",
-      },
-    ]);
-  }
-  function internQ() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the full name of this intern?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the intern's ID number?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the intern's email address?",
-      },
-      {
-        type: "input",
-        name: "school",
-        message:
-          "What school is the intern affiliated with?",
-      },
-    ]);
-  }
-  function moreEmployees() {
-    inquirer
-      .prompt([
-        {
-          type: "confirm",
-          name: "moreEmployees",
-          message:
-            "Would you like to add another employee?",
-        },
-      ])
-      .then((ans) => {
-        if (ans.moreEmployees === true) {
-          roleQ();
-        } else {
-          console.log(
-            "You've built profiles for each of your team members now!"
-          );
-        }
-      });
-  }
-}
-// function employeeQ() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "name",
-//       message: "What is the full name of this employee?",
-//     },
-//     {
-//       type: "input",
-//       name: "id",
-//       message: "What is the employee's ID number?",
-//     },
-//     {
-//       type: "input",
-//       name: "email",
-//       message: "What is the employee's email address?",
-//     },
-//     {
-//       type: "checkbox",
-//       name: "role",
-//       message:
-//         "Which of the following roles does the employee have?",
-//       choices: ["Engineer", "Manager", "Intern"],
-//     },
-//   ]);
-// }
 
-// //Question for Engineer
-// function engineerQ() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "github",
-//       message:
-//         "Please enter the engineer's GitHub profile URL",
-//     },
-//   ]);
-// }
-
-// //Question for the manager
-// function managerQ() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "officeNumber",
-//       message: "What is the manager's office number?",
-//     },
-//   ]);
-// }
-
-// //Question for Intern
-// function internQ() {
-//   return inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "school",
-//       message: "What school is the intern attending?",
-//     },
-//   ]);
-// }
-
-// //Inquirer asks questions
-// async function startQ() {
-//   try {
+// function runTeamBuilder() {
+//   function roleQ() {
+//     inquirer
+//       .prompt({
+//         type: "checkbox",
+//         name: "role",
+//         message:
+//           "Which of the following roles does the employee have?",
+//         choices: ["Engineer", "Manager", "Intern"],
+//       })
+//       .then((ans) => {
+//         if (ans.role[0] === "Engineer") {
+//           engineerQ();
+//         } else if (ans.role[0] === "Manager") {
+//           managerQ();
+//         } else {
+//           internQ();
+//         }
+//       });
 //   }
-// }
-
-// employeeQ()
-//   .then(function (ans) {
-//     if (ans.role[0] === "Engineer") {
-//       engineerQ().then(function (answer) {
+//   function engineerQ() {
+//     inquirer
+//       .prompt([
+//         {
+//           type: "input",
+//           name: "name",
+//           message:
+//             "What is the full name of this engineer?",
+//         },
+//         {
+//           type: "input",
+//           name: "id",
+//           message: "What is the engineer's ID number?",
+//         },
+//         {
+//           type: "input",
+//           name: "email",
+//           message: "What is the engineer's email address?",
+//         },
+//         {
+//           type: "input",
+//           name: "github",
+//           message:
+//             "Please enter the engineer's GitHub profile URL",
+//         },
+//       ])
+//       .then((ans) => {
 //         const engineerObj = new Engineer(
 //           ans.id,
 //           ans.email,
 //           ans.name,
-//           answer.github
+//           ans.github
 //         );
 //         employees.push(engineerObj);
+//         moreEmployees();
 //       });
-//     } else if (ans.role[0] === "Mangager") {
-//       managerQ().then(function (res) {
+//   }
+//   function managerQ() {
+//     inquirer
+//       .prompt([
+//         {
+//           type: "input",
+//           name: "name",
+//           message: "What is the full name of this manager?",
+//         },
+//         {
+//           type: "input",
+//           name: "id",
+//           message: "What is the manager's ID number?",
+//         },
+//         {
+//           type: "input",
+//           name: "email",
+//           message: "What is the manager's email address?",
+//         },
+//         {
+//           type: "input",
+//           name: "officeNumber",
+//           message: "What is the manager's office number?",
+//         },
+//       ])
+//       .then((ans) => {
 //         const managerObj = new Manager(
 //           ans.id,
 //           ans.email,
 //           ans.name,
-//           res.officeNumber
+//           ans.officeNumber
 //         );
 //         employees.push(managerObj);
+//         moreEmployees();
 //       });
-//     } else {
-//       internQ().then(function (response) {
+//   }
+//   function internQ() {
+//     inquirer
+//       .prompt([
+//         {
+//           type: "input",
+//           name: "name",
+//           message: "What is the full name of this intern?",
+//         },
+//         {
+//           type: "input",
+//           name: "id",
+//           message: "What is the intern's ID number?",
+//         },
+//         {
+//           type: "input",
+//           name: "email",
+//           message: "What is the intern's email address?",
+//         },
+//         {
+//           type: "input",
+//           name: "school",
+//           message:
+//             "What school is the intern affiliated with?",
+//         },
+//       ])
+//       .then((ans) => {
 //         const internObj = new Intern(
 //           ans.id,
 //           ans.email,
 //           ans.name,
-//           response.school
+//           ans.school
 //         );
 //         employees.push(internObj);
+//         moreEmployees();
 //       });
-//     }
-//   })
-//   .then(function () {
-//     console.log(employees);
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
+//   }
+//   function moreEmployees() {
+//     inquirer
+//       .prompt({
+//         type: "confirm",
+//         name: "moreEmployees",
+//         message: "Would you like to add another employee?",
+//         default: true,
+//       })
+//       .then((ans) => {
+//         if (ans.moreEmployees === true) {
+//           roleQ();
+//         } else {
+//           writeTeamBuilder();
+//         }
+//       });
+//   }
+
+// }
+//ask user for role
+function roleQ() {
+  return inquirer.prompt({
+    type: "checkbox",
+    name: "role",
+    message:
+      "Which of the following roles does the employee have?",
+    choices: ["Engineer", "Manager", "Intern"],
+  });
+}
+//asks user about engineer
+function engineerQ() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the full name of this engineer?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email address?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message:
+        "Please enter the engineer's GitHub profile URL",
+    },
+  ]);
+}
+//asks user about manager
+function managerQ() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the full name of this manager?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's office number?",
+    },
+  ]);
+}
+//asks user about intern
+function internQ() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the full name of this intern?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email address?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What school is the intern affiliated with?",
+    },
+  ]);
+}
+//asks user if they want to input another employee
+function moreEmployees() {
+  return inquirer.prompt({
+    type: "confirm",
+    name: "moreEmployees",
+    message: "Would you like to add another employee?",
+    default: true,
+  });
+}
+//Runs through user input to build team
+function runTeamBuilder() {
+  roleQ()
+    .then(function (data) {
+      if (data.role[0] === "Engineer") {
+        engineerQ().then(function (ans) {
+          const engineerObj = new Engineer(
+            ans.name,
+            ans.id,
+            ans.email,
+            ans.github
+          );
+          employees.push(engineerObj);
+          writeTeamBuilder();
+          moreEmployees().then(function (ans) {
+            if (ans.moreEmployees === true) {
+              runTeamBuilder();
+            }
+          });
+        });
+      } else if (data.role[0] === "Manager") {
+        managerQ().then(function (ans) {
+          const managerObj = new Manager(
+            ans.name,
+            ans.id,
+            ans.email,
+            ans.github
+          );
+          employees.push(managerObj);
+          writeTeamBuilder();
+          moreEmployees().then(function (ans) {
+            if (ans.moreEmployees === true) {
+              runTeamBuilder();
+            }
+          });
+        });
+      } else if (data.role[0] === "Intern") {
+        internQ().then(function (ans) {
+          const internObj = new Intern(
+            ans.name,
+            ans.id,
+            ans.email,
+            ans.github
+          );
+          employees.push(internObj);
+          writeTeamBuilder();
+          moreEmployees().then(function (ans) {
+            if (ans.moreEmployees === true) {
+              runTeamBuilder();
+            }
+          });
+        });
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function writeTeamBuilder() {
+  const html = render(employees);
+  asyncWriteFile(outputPath, html);
+}
+runTeamBuilder();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
